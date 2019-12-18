9cf8214d847654e15f9a8de06adadb6cf8a5c156
FrankKamala <frankkamala@gmail.com>
2019-12-18 11:03:13 +0300

    add Instanciating Test

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,11 @@ public class PostTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
     }
 }
